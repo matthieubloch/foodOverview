7d95ee0320c6c6e9cbc7bfefbd885dc69c458f77
Matthieu Bloch <matthieu.k.bloch@gmail.com>
2014-05-26 23:44:48 +0200

    Add test for complex menus

--- tests/TestChargementMenu.py
+++ tests/TestChargementMenu.py
@@ -1,27 +1,41 @@
 from nose.tools import *
 from foodOverview.Menu import Menu
 
+ressources = './tests/ressources/'
+menu_simple = 'menu_simple.yml'
+menu_complexe = 'menu_complexe.yml'
+
+fichierMenu = ''
+menu = ''
 
 def testChargementMenuSimple():
-    fichierMenu = givenUnMenuSimple()
-    menu = whenLeMenuEstCharge(fichierMenu)
+    givenUnMenuSimple()
+    menu = whenLeMenuEstCharge()
 
-    thenLeMenuContient('crepe salee', menu)
-    thenLeMenuContient('sandwich', menu)
-    thenLeMenuContient('canette', menu)
+    thenLeMenuContient('crepe salee')
+    thenLeMenuContient('sandwich')
+    thenLeMenuContient('canette')
 
-    thenLeProduitCoute('sandwich', 3, menu)
-    thenLeProduitCoute('crepe salee', 1.5, menu)
+    thenLeProduitCoute('sandwich', 3)
+    thenLeProduitCoute('crepe salee', 1.5)
+
+def testChargementProduitComposite():
+    givenUnMenuComplexe()
 
 def givenUnMenuSimple():
-    return './tests/ressources/menu_simple.yml'
+    global fichierMenu
+    fichierMenu = ressources + menu_simple
 
-def whenLeMenuEstCharge(fichierMenu):
-    return Menu(fichierMenu)
+def givenUnMenuComplexe():
+    global fichierMenu
+    fichierMenu = ressources + menu_complexe
 
-def thenLeMenuContient(produit, menu):
-    print menu.getProducts()
+def whenLeMenuEstCharge():
+    global menu
+    menu = Menu(fichierMenu)
+
+def thenLeMenuContient(produit):
     assert_true(menu.findProduct(produit) != None)
 
-def thenLeProduitCoute(produit, prix, menu):
+def thenLeProduitCoute(produit, prix):
     assert_equal(menu.findProduct(produit).getPrix(), prix)
